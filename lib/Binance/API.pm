1182eb7ab68d8f175bad42889bf9c7f4d54e10eb
Filip la Gre <tutenhamond@gmail.com>
2018-05-19 03:23:09 +0200

    Update API.pm

--- lib/Binance/API.pm
+++ lib/Binance/API.pm
@@ -104,97 +104,105 @@ sub new {
     bless $self, $class;
 }
 
-=head2 aggregate_trades
+=head2 ping
 
-my $aggTrades = $api->aggregate_trades( symbol => 'ETHBTC' );
+$api->ping();
 
-Gets compressed, aggregate trades. Trades that fill at the time, from the same
-order, with the same price will have the quantity aggregated
-
-PARAMETERS:
-- symbol    [REQUIRED] Symbol, for example ETHBTC
-- fromId    [OPTIONAL] ID to get aggregate trades from INCLUSIVE
-- startTime [OPTIONAL] timestamp in ms to get aggregate trades from INCLUSIVE
-- endTime   [OPTIONAL] timestamp in ms to get aggregate trades until INCLUSIVE
-- limit     [OPTIONAL] Default 500; max 500.
-
-RETURNS:
-    An array of HASHrefs:
-
-    [
-      {
-        "a": 26129,         // Aggregate tradeId
-        "p": "0.01633102",  // Price
-        "q": "4.70443515",  // Quantity
-        "f": 27781,         // First tradeId
-        "l": 27781,         // Last tradeId
-        "T": 1498793709153, // Timestamp
-        "m": true,          // Was the buyer the maker?
-        "M": true           // Was the trade the best price match?
-      }
-    ]
-
-    (Source: Binance API documentation)
-
-=cut
-
-sub aggregate_trades {
-    my ($self, %params) = @_;
-
-    unless ($params{'symbol'}) {
-        $self->log->error('Parameter "symbol" required');
-        Binance::Exception::Parameter::Required->throw(
-            error => 'Parameter "symbol" required',
-            parameters => ['symbol']
-        );
-    }
-
-    my $query = {
-        symbol    => $params{'symbol'},
-        fromId    => $params{'fromId'},
-        startTime => $params{'startTime'},
-        endTime   => $params{'endTime'},
-        limit     => $params{'limit'},
-    };
-
-    return $self->ua->get('/api/v1/aggTrades', { query => $query } );
-}
-
-=head2 all_book_tickers
-
-my $all_book_tickers = $api->all_book_tickers();
-
-Best price/qty on the order book for all symbols
+Test connectivity to the Rest API
 
 PARAMETERS:
     NONE
 
 RETURNS:
-    An array of HASHrefs:
-
-    [
-      {
-        "symbol": "LTCBTC",
-        "bidPrice": "4.00000000",
-        "bidQty": "431.00000000",
-        "askPrice": "4.00000200",
-        "askQty": "9.00000000"
-      },
-      {
-        "symbol": "ETHBTC",
-        "bidPrice": "0.07946700",
-        "bidQty": "9.00000000",
-        "askPrice": "100000.00000000",
-        "askQty": "1000.00000000"
-      }
-    ]
-
-    (Source: Binance API documentation)
+1 if successful, otherwise 0
 
 =cut
 
-sub all_book_tickers {
-    return $_[0]->ua->get('/api/v1/ticker/allBookTickers');
+sub ping {
+    return keys %{$_[0]->ua->get('/api/v1/ping')} == 0 ? 1 : 0;
+}
+
+=head2 Check server time
+
+$api->time();
+
+Test connectivity to the Rest API and get the current server time.
+
+PARAMETERS:
+    NONE
+
+RETURNS:
+    Server (epoch) time in milliseconds
+
+=cut
+
+sub time {
+    my $self = shift;
+
+    my $time = $self->ua->get('/api/v1/time');
+    return exists $time->{serverTime} ? $time->{serverTime} : 0;
+}
+
+=head2 Exchange information
+
+$api->exchangeInfo();
+
+Current exchange trading rules and symbol information
+
+PARAMETERS:
+    NONE
+
+RETURNS:
+{
+  "timezone": "UTC",
+  "serverTime": 1508631584636,
+  "rateLimits": [{
+      "rateLimitType": "REQUESTS",
+      "interval": "MINUTE",
+      "limit": 1200
+    },
+    {
+      "rateLimitType": "ORDERS",
+      "interval": "SECOND",
+      "limit": 10
+    },
+    {
+      "rateLimitType": "ORDERS",
+      "interval": "DAY",
+      "limit": 100000
+    }
+  ],
+  "exchangeFilters": [],
+  "symbols": [{
+    "symbol": "ETHBTC",
+    "status": "TRADING",
+    "baseAsset": "ETH",
+    "baseAssetPrecision": 8,
+    "quoteAsset": "BTC",
+    "quotePrecision": 8,
+    "orderTypes": ["LIMIT", "MARKET"],
+    "icebergAllowed": false,
+    "filters": [{
+      "filterType": "PRICE_FILTER",
+      "minPrice": "0.00000100",
+      "maxPrice": "100000.00000000",
+      "tickSize": "0.00000100"
+    }, {
+      "filterType": "LOT_SIZE",
+      "minQty": "0.00100000",
+      "maxQty": "100000.00000000",
+      "stepSize": "0.00100000"
+    }, {
+      "filterType": "MIN_NOTIONAL",
+      "minNotional": "0.00100000"
+    }]
+  }]
+}
+
+=cut
+
+sub exchangeInfo {
+    return $_[0]->ua->get('/api/v1/ticker/exchangeInfo');
 }
 
 =head2 all_prices
@@ -280,6 +288,150 @@ sub depth {
     return $self->ua->get('/api/v1/depth', { query => $query } );
 }
 
+=head2 Recent trades list
+
+$api->trades();
+
+Get recent trades (up to last 500).
+
+PARAMETERS:
+- symbol           [REQUIRED]
+- limit            [OPTIONAL] Default 500; max 500.
+
+RETURNS:
+[
+  {
+    "id": 28457,
+    "price": "4.00000100",
+    "qty": "12.00000000",
+    "time": 1499865549590,
+    "isBuyerMaker": true,
+    "isBestMatch": true
+  }
+]
+
+=cut
+
+sub trades {
+    my ($self, %params) = @_;
+
+    unless ($params{'symbol'}) {
+        $self->log->error('Parameter "symbol" required');
+        Binance::Exception::Parameter::Required->throw(
+            error => 'Parameter "symbol" required',
+            parameters => ['symbol']
+        );
+    }
+
+    my $query = {
+        symbol    => $params{'symbol'},
+        limit     => $params{'limit'},
+    };
+
+    return $self->ua->get('/api/v1/trades', { query => $query } );
+}
+
+=head2 Old trade lookup (MARKET_DATA)
+
+$api->historicalTrades();
+
+Get older trades.
+
+PARAMETERS:
+- symbol           [REQUIRED]
+- limit            [OPTIONAL] Default 500; max 500.
+- fromId           [OPTIONAL] TradeId to fetch from. Default gets most recent trades.
+
+RETURNS:
+[
+  {
+    "id": 28457,
+    "price": "4.00000100",
+    "qty": "12.00000000",
+    "time": 1499865549590,
+    "isBuyerMaker": true,
+    "isBestMatch": true
+  }
+]
+
+=cut
+
+sub historicalTrades {
+    my ($self, %params) = @_;
+
+    unless ($params{'symbol'}) {
+        $self->log->error('Parameter "symbol" required');
+        Binance::Exception::Parameter::Required->throw(
+            error => 'Parameter "symbol" required',
+            parameters => ['symbol']
+        );
+    }
+
+    my $query = {
+        symbol    => $params{'symbol'},
+        limit     => $params{'limit'},
+        fromId    => $params{'fromId'},
+    };
+
+    return $self->ua->get('/api/v1/historicalTrades', { query => $query } );
+}
+
+=head2 aggregate_trades
+
+my $aggTrades = $api->aggregate_trades( symbol => 'ETHBTC' );
+
+Gets compressed, aggregate trades. Trades that fill at the time, from the same
+order, with the same price will have the quantity aggregated
+
+PARAMETERS:
+- symbol    [REQUIRED] Symbol, for example ETHBTC
+- fromId    [OPTIONAL] ID to get aggregate trades from INCLUSIVE
+- startTime [OPTIONAL] timestamp in ms to get aggregate trades from INCLUSIVE
+- endTime   [OPTIONAL] timestamp in ms to get aggregate trades until INCLUSIVE
+- limit     [OPTIONAL] Default 500; max 500.
+
+RETURNS:
+    An array of HASHrefs:
+
+    [
+      {
+        "a": 26129,         // Aggregate tradeId
+        "p": "0.01633102",  // Price
+        "q": "4.70443515",  // Quantity
+        "f": 27781,         // First tradeId
+        "l": 27781,         // Last tradeId
+        "T": 1498793709153, // Timestamp
+        "m": true,          // Was the buyer the maker?
+        "M": true           // Was the trade the best price match?
+      }
+    ]
+
+    (Source: Binance API documentation)
+
+=cut
+
+sub aggregate_trades {
+    my ($self, %params) = @_;
+
+    unless ($params{'symbol'}) {
+        $self->log->error('Parameter "symbol" required');
+        Binance::Exception::Parameter::Required->throw(
+            error => 'Parameter "symbol" required',
+            parameters => ['symbol']
+        );
+    }
+
+    my $query = {
+        symbol    => $params{'symbol'},
+        fromId    => $params{'fromId'},
+        startTime => $params{'startTime'},
+        endTime   => $params{'endTime'},
+        limit     => $params{'limit'},
+    };
+
+    return $self->ua->get('/api/v1/aggTrades', { query => $query } );
+}
+
 =head2 klines
 
 my $klines = $api->klines( symbol => 'ETHBTC', interval => '1M' );
@@ -346,6 +498,164 @@ sub klines {
     };
 
     return $self->ua->get('/api/v1/klines', { query => $query } );
+}
+
+=head2 ticker
+
+my $ticker = $api->klines( symbol => 'ETHBTC', interval => '1M' );
+
+24 hour price change statistics
+
+PARAMETERS:
+- symbol [REQUIRED] Symbol, for example ETHBTC
+
+RETURNS:
+    A HASHref:
+
+    {
+      "priceChange": "-94.99999800",
+      "priceChangePercent": "-95.960",
+      "weightedAvgPrice": "0.29628482",
+      "prevClosePrice": "0.10002000",
+      "lastPrice": "4.00000200",
+      "bidPrice": "4.00000000",
+      "askPrice": "4.00000200",
+      "openPrice": "99.00000000",
+      "highPrice": "100.00000000",
+      "lowPrice": "0.10000000",
+      "volume": "8913.30000000",
+      "openTime": 1499783499040,
+      "closeTime": 1499869899040,
+      "fristId": 28385,   // First tradeId
+      "lastId": 28460,    // Last tradeId
+      "count": 76         // Trade count
+    }
+
+    (Source: Binance API documentation)
+
+=cut
+
+sub ticker {
+    my ($self, %params) = @_;
+
+    unless ($params{'symbol'}) {
+        $self->log->error('Parameter "symbol" required');
+        Binance::Exception::Parameter::Required->throw(
+            error => 'Parameter "symbol" required',
+            parameters => ['symbol']
+        );
+    }
+
+    my $query = {
+        symbol    => $params{'symbol'},
+    };
+
+    return $self->ua->get('/api/v1/ticker/24hr', { query => $query } );
+}
+
+=head2 Symbol price ticker
+
+$api->tickerPrice();
+
+Latest price for a symbol or symbols.
+
+PARAMETERS:
+- symbol            [OPTIONAL]
+
+RETURNS:
+{
+  "symbol": "LTCBTC",
+  "price": "4.00000200"
+}
+OR
+[
+  {
+    "symbol": "LTCBTC",
+    "price": "4.00000200"
+  },
+  {
+    "symbol": "ETHBTC",
+    "price": "0.07946600"
+  }
+]
+
+=cut
+
+sub tickerPrice {
+    my ($self, %params) = @_;
+
+    my $query = {
+        symbol    => $params{'symbol'},
+    };
+
+    return $self->ua->get('/api/v3/ticker/price', { query => $query } );
+}
+
+=head2 all_book_tickers
+
+my $all_book_tickers = $api->all_book_tickers();
+
+Best price/qty on the order book for all symbols
+
+PARAMETERS:
+    NONE
+
+RETURNS:
+    An array of HASHrefs:
+
+    [
+      {
+        "symbol": "LTCBTC",
+        "bidPrice": "4.00000000",
+        "bidQty": "431.00000000",
+        "askPrice": "4.00000200",
+        "askQty": "9.00000000"
+      },
+      {
+        "symbol": "ETHBTC",
+        "bidPrice": "0.07946700",
+        "bidQty": "9.00000000",
+        "askPrice": "100000.00000000",
+        "askQty": "1000.00000000"
+      }
+    ]
+
+    (Source: Binance API documentation)
+
+=cut
+
+sub all_book_tickers {
+    return $_[0]->ua->get('/api/v1/ticker/allBookTickers');
+}
+
+=head2 Symbol order book ticker
+
+$api->bookTicker();
+
+Best price/qty on the order book for a symbol or symbols.
+
+PARAMETERS:
+- symbol            [OPTIONAL]
+
+RETURNS:
+{
+  "symbol": "LTCBTC",
+  "bidPrice": "4.00000000",
+  "bidQty": "431.00000000",
+  "askPrice": "4.00000200",
+  "askQty": "9.00000000"
+}
+
+=cut
+
+sub bookTicker {
+    my ($self, %params) = @_;
+
+    my $query = {
+        symbol    => $params{'symbol'},
+    };
+
+    return $self->ua->get('/api/v1/bookTicker', { query => $query } );
 }
 
 =head2 order
@@ -416,60 +726,82 @@ sub order {
     return $self->ua->post('/api/v3/order', { signed => 1, body => $body } );
 }
 
-=head2 ping
+=head2 Test new order (TRADE)
 
-$api->ping();
+$api->orderTest();
 
-Test connectivity to the Rest API
+Test new order creation and signature/recvWindow long. Creates and validates a new order but does not send it into the matching engine.
 
 PARAMETERS:
-    NONE
+- symbol           [REQUIRED] Symbol, for example ETHBTC
+- side             [REQUIRED] BUY or SELL
+- type             [REQUIRED] LIMIT or MARKET
+- timeInForce      [REQUIRED] GTC or IOC
+- quantity         [REQUIRED] Quantity (of symbols) in order
+- price            [REQUIRED] Price (of symbol) in order
+- newClientOrderId [OPTIONAL] A unique id for the order. Automatically generated
+                              if not sent.
+- stopPrice        [OPTIONAL] Used with stop orders
+- icebergQty       [OPTIONAL] Used with iceberg orders
 
 RETURNS:
-    1 if successful, otherwise 0
+{}
 
 =cut
 
-sub ping {
-    return keys %{$_[0]->ua->get('/api/v1/ping')} == 0 ? 1 : 0;
-}
+sub orderTest {
+    my ($self, %params) = @_;
 
-=head2 ticker
-
-my $ticker = $api->klines( symbol => 'ETHBTC', interval => '1M' );
-
-24 hour price change statistics
-
-PARAMETERS:
-- symbol [REQUIRED] Symbol, for example ETHBTC
-
-RETURNS:
-    A HASHref:
-
-    {
-      "priceChange": "-94.99999800",
-      "priceChangePercent": "-95.960",
-      "weightedAvgPrice": "0.29628482",
-      "prevClosePrice": "0.10002000",
-      "lastPrice": "4.00000200",
-      "bidPrice": "4.00000000",
-      "askPrice": "4.00000200",
-      "openPrice": "99.00000000",
-      "highPrice": "100.00000000",
-      "lowPrice": "0.10000000",
-      "volume": "8913.30000000",
-      "openTime": 1499783499040,
-      "closeTime": 1499869899040,
-      "fristId": 28385,   // First tradeId
-      "lastId": 28460,    // Last tradeId
-      "count": 76         // Trade count
+    my @required = ('symbol', 'side', 'type', 'timeInForce', 'quantity', 'price');
+    foreach my $param (@required) {
+        unless (defined ($params{$param})) {
+            $self->log->error('Parameter "'.$param.'" required');
+            Binance::Exception::Parameter::Required->throw(
+                error => 'Parameter "'.$param.'" required',
+                parameters => [$param]
+            );
+        }
     }
 
-    (Source: Binance API documentation)
+    my $body = {
+        symbol           => $params{'symbol'},
+        side             => $params{'side'},
+        type             => $params{'type'},
+        timeInForce      => $params{'timeInForce'},
+        quantity         => $params{'quantity'},
+        price            => $params{'price'},
+        newClientOrderId => $params{'newClientOrderId'},
+        stopPrice        => $params{'stopPrice'},
+        icebergQty       => $params{'icebergQty'},
+    };
+
+    return $self->ua->post('/api/v3/order/test', { signed => 1, body => $body } );
+}
+
+=head2 Cancel order (TRADE)
+
+$api->cancelOrder();
+
+Cancel an active order.
+
+PARAMETERS:
+- symbol             [REQUIRED]
+- orderId            [OPTIONAL]
+- origClientOrderId  [OPTIONAL]
+- newClientOrderId   [OPTIONAL] Used to uniquely identify this cancel. Automatically generated by default.
+- recvWindow         [OPTIONAL]
+
+RETURNS:
+{
+  "symbol": "LTCBTC",
+  "origClientOrderId": "myOrder1",
+  "orderId": 1,
+  "clientOrderId": "cancelMyOrder1"
+}
 
 =cut
 
-sub ticker {
+sub cancelOrder {
     my ($self, %params) = @_;
 
     unless ($params{'symbol'}) {
@@ -480,32 +812,67 @@ sub ticker {
         );
     }
 
-    my $query = {
-        symbol    => $params{'symbol'},
+    my $body = {
+        symbol             => $params{'symbol'},
+        orderId            => $params{'orderId'},
+        origClientOrderId  => $params{'origClientOrderId'},
+        newClientOrderId   => $params{'newClientOrderId'},
+        recvWindow         => $params{'recvWindow'},
     };
 
-    return $self->ua->get('/api/v1/ticker/24hr', { query => $query } );
+    return $self->ua->delete('/api/v3/order', { signed => 1, body => $body } );
 }
 
-=head2 time
+=head2 All orders (USER_DATA)
 
-$api->ping();
+$api->allOrders();
 
-Test connectivity to the Rest API and get the current server time.
+Get all account orders; active, canceled, or filled.
 
 PARAMETERS:
-    NONE
+- symbol             [REQUIRED]
+- orderId            [OPTIONAL]
+- limit              [OPTIONAL] Default 500; max 500.
+- recvWindow         [OPTIONAL]
 
 RETURNS:
-    Server (epoch) time in milliseconds
+[
+  {
+    "symbol": "LTCBTC",
+    "orderId": 1,
+    "clientOrderId": "myOrder1",
+    "price": "0.1",
+    "origQty": "1.0",
+    "executedQty": "0.0",
+    "status": "NEW",
+    "timeInForce": "GTC",
+    "type": "LIMIT",
+    "side": "BUY",
+    "stopPrice": "0.0",
+    "icebergQty": "0.0",
+    "time": 1499827319559,
+    "isWorking": true
+  }
+]
 
 =cut
 
-sub time {
-    my $self = shift;
-
-    my $time = $self->ua->get('/api/v1/time');
-    return exists $time->{serverTime} ? $time->{serverTime} : 0;
+sub allOrders {
+    my ($self, %params) = @_;
+    unless ($params{'symbol'}) {
+        $self->log->error('Parameter "symbol" required');
+        Binance::Exception::Parameter::Required->throw(
+            error => 'Parameter "symbol" required',
+            parameters => ['symbol']
+        );
+    }
+    my $query = {
+        symbol     => $params{'symbol'},
+        orderId    => $params{'orderId'},
+        limit      => $params{'limit'},
+        recvWindow => $params{'recvWindow'},
+    };
+    return $self->ua->get('/api/v3/allOrders', { signed => 1, query => $query } );
 }
 
 sub log { return $_[0]->{logger}; }
