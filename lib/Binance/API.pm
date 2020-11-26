6aea9627b435ec3ebf8cb24a4b974ade56c0789c
Lari Taskula <lari.taskula@hypernova.fi>
2020-11-26 03:01:26 +0200

    Release 1.05

--- lib/Binance/API.pm
+++ lib/Binance/API.pm
@@ -36,7 +36,7 @@ use Binance::API::Request;
 use Binance::Exception::Parameter::BadValue;
 use Binance::Exception::Parameter::Required;
 
-our $VERSION = '1.04';
+our $VERSION = '1.05';
 
 =head1 NAME
 
